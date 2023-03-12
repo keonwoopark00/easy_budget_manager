f608e2de4fc3e08adec31cb33aa48e1fe6aac10f
Keonwoo Park <keonwoo.park@injoy.bio>
2023-03-12 14:30:12 -0400

    Update github ci and test

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:kkb/main.dart';
+import 'package:budget_manager/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const BudgetManager());
+    await tester.pumpWidget(const MainApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
